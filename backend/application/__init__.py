1adef2b663a3467d5bee89cd63eb087886e177e0
hhscc <lzzzii10leo@gmail.com>
2025-12-26 13:37:08 +0800

    Linux 中不能用mysqlclient

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -2,4 +2,8 @@
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app
 
+# 使用 PyMySQL 替代 mysqlclient
+import pymysql
+pymysql.install_as_MySQLdb()
+
 __all__ = ('celery_app',)
